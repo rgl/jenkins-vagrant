2cd12753df1e2b770cf9b7d61892f593dd04afd3
Rui Lopes <rgl@ruilopes.com>
2017-05-23 06:01:44 +0100

    also show the .net exception on the global powershell trap

--- windows/ps.ps1
+++ windows/ps.ps1
@@ -13,6 +13,7 @@ $ErrorActionPreference = 'Stop'
 trap {
     Write-Output "ERROR: $_"
     Write-Output (($_.ScriptStackTrace -split '\r?\n') -replace '^(.*)$','ERROR: $1')
+    Write-Output (($_.Exception.ToString() -split '\r?\n') -replace '^(.*)$','ERROR EXCEPTION: $1')
     Exit 1
 }
 
