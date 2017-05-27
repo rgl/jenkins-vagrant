535873e444309f5bdb1e292d6d95f1b77c24809d
Rui Lopes <rgl@ruilopes.com>
2017-05-27 19:35:10 +0100

    also show the created jobs in the install summary

--- provision.sh
+++ provision.sh
@@ -783,6 +783,9 @@ Jenkins.instance.nodes.sort { it.name }.each {
 println "jenkins master node"
 Jenkins.instance.assignedLabels.sort().each { println "jenkins master node label: " + it }
 User.all.sort { it.id }.each { println sprintf("jenkins user: %s (%s)", it.id, it.fullName) }
+Jenkins.instance.getAllItems(AbstractItem.class).sort { it.fullName }.each {
+    println sprintf("jenkins job: %s (%s)", it.fullName, it.class)
+}
 EOF
 echo "jenkins is installed at https://jenkins.example.com"
 echo "the admin password is $(cat /var/lib/jenkins/secrets/initialAdminPassword)"
