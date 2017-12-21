e2876067de696653993bb25fdd053d8a63efe820
Rui Lopes <rgl@ruilopes.com>
2017-12-21 22:35:30 +0000

    upgrade to OpenCover 4.6.819

--- windows/opencover.portable/tools/chocolateyInstall.ps1
+++ windows/opencover.portable/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
-# NB this version came from https://github.com/opencover/opencover/commit/2010793db8e4288accaf3484a76e072cb253eac8
-$url = 'https://ci.appveyor.com/api/buildjobs/v57oxenhb8sogf6p/artifacts/main%2Fbin%2Fzip%2Fopencover.4.6.796.zip'
-$checksum = '444a336ee4049f1ee5e9c305942a8988c3974fb9679aeaa0d9b611e471f0feb9'
+# NB this version came from https://github.com/opencover/opencover/commit/ddb4437a731849f9fda6454e4c50ce4774517fd9
+$url = 'https://ci.appveyor.com/api/buildjobs/69583xv18l3i0580/artifacts/main%2Fbin%2Fzip%2Fopencover.4.6.819.zip'
+$checksum = 'b30353730d7757651fbab6bfbcf2443f04d5e648800130c7832ba0d3135a69b7'
 $installPath = "$env:ChocolateyPackageFolder\tools"
 
 Install-ChocolateyZipPackage `
