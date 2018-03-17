4663ba4f55b058864ae53d64023a0a7fc98a5ae6
Rui Lopes <rgl@ruilopes.com>
2018-03-17 16:06:22 +0000

    upgrade to .net core sdk 2.1.101 (includes .net core 2.0.6)

--- windows/provision-dotnetcore-sdk.ps1
+++ windows/provision-dotnetcore-sdk.ps1
@@ -1,12 +1,12 @@
-# see https://github.com/dotnet/core/blob/master/release-notes/2.0/2.0.5.md
+# see https://github.com/dotnet/core/blob/master/release-notes/2.0/2.0.6.md
 
 # opt-out from dotnet telemetry.
 [Environment]::SetEnvironmentVariable('DOTNET_CLI_TELEMETRY_OPTOUT', '1', 'Machine')
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 
 # install the dotnet sdk.
-$archiveUrl = 'https://download.microsoft.com/download/1/1/5/115B762D-2B41-4AF3-9A63-92D9680B9409/dotnet-sdk-2.1.4-win-x64.exe'
-$archiveHash = '10309b343be6dc51b04d09ed97035ddaabe250685e1799fec8a0d3f7f34ea77dddc1d742544320c21bacfa4a2c27678d262540ecf2cd69dad354a8ad207f388d'
+$archiveUrl = 'https://download.microsoft.com/download/D/C/F/DCFA73BE-93CE-4DA0-AB76-98972FD6E475/dotnet-sdk-2.1.101-win-x64.exe'
+$archiveHash = 'c7fb50ca82acba7e9454d53bf9c47a240611f731ea4a095957dfe58ab0404d4f98585951258fb353b5ff16a808dc89931f30b6b8a0ea6e1c967f2e84ba6ee101'
 $archiveName = Split-Path -Leaf $archiveUrl
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host "Downloading $archiveName..."
