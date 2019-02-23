25db86a831879b07406d296c8b730f1385c75e9d
Rui Lopes <rgl@ruilopes.com>
2019-02-23 06:56:35 +0000

    upgrade to .net core sdk 2.1.504 (includes .net core 2.1.8)

--- windows/provision-dotnetcore-sdk.ps1
+++ windows/provision-dotnetcore-sdk.ps1
@@ -1,13 +1,13 @@
 # see https://dotnet.microsoft.com/download/dotnet-core/2.1
-# see https://github.com/dotnet/core/blob/master/release-notes/2.1/2.1.7/2.1.7.md
+# see https://github.com/dotnet/core/blob/master/release-notes/2.1/2.1.8/2.1.8.md
 
 # opt-out from dotnet telemetry.
 [Environment]::SetEnvironmentVariable('DOTNET_CLI_TELEMETRY_OPTOUT', '1', 'Machine')
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 
 # install the dotnet sdk.
-$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/9afc36fb-48ba-43ae-9f37-df634383ae84/2c1329b490c8d4b2787164286bde115c/dotnet-sdk-2.1.503-win-x64.exe'
-$archiveHash = '338fe1d8552d871a4d0716c3e3c044826e723466cd8565e22863d1976e32b7334c8e299fe04597c7cd323066a1150041a7c97bd29ec88ff836b3339e16af55bc'
+$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/1dcd5e24-25da-40dc-9996-4db675c1c22d/e154ee2beeb1eec4e9b049a8d3740c80/dotnet-sdk-2.1.504-win-x64.exe'
+$archiveHash = 'ac2c45dc10428eb888d80b15e1cf8b0ff2e76980038af312c76e8929501a44b0631a9c3c15f098f288c0be4cfd19343ede83def5cda6b16985b23de14b6f4c8c'
 $archiveName = Split-Path -Leaf $archiveUrl
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host "Downloading $archiveName..."
