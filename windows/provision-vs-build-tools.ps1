fe4eec942fef3e419c47d1863ae64a435419d3b9
Rui Lopes <rgl@ruilopes.com>
2019-07-13 08:27:33 +0100

    upgrade to vs build tools 16.1.6

--- windows/provision-vs-build-tools.ps1
+++ windows/provision-vs-build-tools.ps1
@@ -46,15 +46,15 @@ Install-ModifiedChocolateyPackage netfx-4.7.1-devpack 4.7.2558.0 e293769f03da7a4
 # add support for building applications that target the .net 4.5.2 framework.
 choco install -y netfx-4.5.2-devpack
 
-# install the Visual Studio Build Tools 2019 16.1.3.
+# install the Visual Studio Build Tools 2019 16.1.6.
 # see https://www.visualstudio.com/downloads/
 # see https://docs.microsoft.com/en-us/visualstudio/releases/2019/release-notes
 # see https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio?view=vs-2019
 # see https://docs.microsoft.com/en-us/visualstudio/install/command-line-parameter-examples?view=vs-2019
 # see https://docs.microsoft.com/en-us/visualstudio/install/workload-and-component-ids?view=vs-2019
 # see https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-build-tools?view=vs-2019
-$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/e48a4988-6941-44cd-b10d-869ae6e5a6e4/ebb9b691a4ac5916171f319a7863d5a5/vs_buildtools.exe'
-$archiveHash = 'aaafae424eb9f8c7d9f5ab07f63cf3050ace3c1270dd0d31c8fc0e8831f35011'
+$archiveUrl = 'https://download.visualstudio.microsoft.com/download/pr/c3f86bc6-1398-4fbc-8bd9-b2c242108344/327c0bd70716b92dc7f968aadaf9a502/vs_buildtools.exe'
+$archiveHash = '65c61b25408253a8009ee9100878d0eb99bfa7f483fef9ee708db1deb6419347'
 $archiveName = Split-Path $archiveUrl -Leaf
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host 'Downloading the Visual Studio Build Tools Setup Bootstrapper...'
