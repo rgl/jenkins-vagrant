8ac63a9569f1ddda325fb32ff80b7d5111148bc3
Rui Lopes <rgl@ruilopes.com>
2019-07-27 13:54:56 +0100

    upgrade to unity 2019.1.12

--- windows/provision-unity.ps1
+++ windows/provision-unity.ps1
@@ -10,6 +10,6 @@ Write-Output 'Installing Unity Hub...'
 &$archivePath /S | Out-String -Stream
 
 Write-Output 'Installing Unity Editor...'
-choco install -y unity --version 2019.1.0.20190417
+choco install -y unity --version 2019.1.12
 
 Write-Output 'Unity is installed, but You still need to manually activate it using the Unity Hub. See the README.md file.'
