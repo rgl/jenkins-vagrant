d0e5aac32b228b38273ec8e4b548e2a0c16eeac8
Rui Lopes <rgl@ruilopes.com>
2026-03-21 07:21:21 +0000

    upgrade to docker compose 5.1.1

--- windows/provision-docker-compose.ps1
+++ windows/provision-docker-compose.ps1
@@ -1,13 +1,13 @@
 # download install the docker-compose binaries.
 # see https://github.com/docker/compose/releases
 # renovate: datasource=github-releases depName=docker/compose
-$archiveVersion = '5.0.1'
+$archiveVersion = '5.1.1'
 $archiveUrl = "https://github.com/docker/compose/releases/download/v$archiveVersion/docker-compose-windows-x86_64.exe"
 $archiveName = Split-Path -Leaf $archiveUrl
 $archivePath = "$env:TEMP\$archiveName"
 Write-Host "Installing docker-compose $archiveVersion..."
 (New-Object System.Net.WebClient).DownloadFile($archiveUrl, $archivePath)
-$dockerCliPluginsPath = "$env:ProgramData\docker\cli-plugins"
+$dockerCliPluginsPath = "$env:ProgramFiles\Docker\cli-plugins"
 mkdir -Force $dockerCliPluginsPath | Out-Null
 Move-Item -Force $archivePath "$dockerCliPluginsPath\docker-compose.exe"
 # ensure that all the plugins inherit the parent directory permissions.
